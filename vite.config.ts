d0d43c39fbd7a39898cbd1e08e2153db8b1f443a
Sávio Mendes <saviomd@users.noreply.github.com>
2024-11-24 16:34:55 -0300

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/test-router-prerender/",
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
